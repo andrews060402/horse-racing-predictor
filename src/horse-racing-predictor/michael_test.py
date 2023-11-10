0daf77b0b9612614b51e7430f9a050d1453033a5
chungmjvt <chungmj@vt.edu>
2023-11-10 16:50:41 -0500

    update models

--- src/horse-racing-predictor/michael_test.py
+++ src/horse-racing-predictor/michael_test.py
@@ -2,13 +2,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
 
-
-attributes = ['age', 'weight', 'sex', 'start_position', 'weather', 'track_conditions']
+attributes = ['age', 'weight', 'sex', 'start_position', 'weather', 'track_conditions', 'finish_position']
 # Step 1: Load the CSV file into a pandas DataFrame
 df = pd.read_csv('../../data/woodbine_horses.csv', usecols=attributes)
-
-
 
 # Assuming 'sex', 'weather', and 'track_conditions' are categorical
 categorical_attributes = ['sex', 'weather', 'track_conditions']
@@ -28,7 +29,33 @@ df[numerical_attributes] = scaler.fit_transform(df[numerical_attributes])
 
 # Splitting the Data into Training and Testing Sets
 # Assuming you have a target variable named 'outcome'
-X = df.drop('outcome', axis=1)  # Replace 'outcome' with your actual target column
-y = df['outcome']  # Replace with actual target column
+X = df.drop('finish_position', axis=1)
+y = df['finish_position']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+# MODEL TRAINING AND EVALUATION
+# Initialize models
+svm = SVC()
+mlp = MLPClassifier()
+logistic = LogisticRegression()
+
+# Train models
+svm.fit(X_train, y_train)
+mlp.fit(X_train, y_train)
+logistic.fit(X_train, y_train)
+
+# Make predictions
+svm_preds = svm.predict(X_test)
+mlp_preds = mlp.predict(X_test)
+logistic_preds = logistic.predict(X_test)
+
+# Evaluate models
+svm_f1 = f1_score(y_test, svm_preds)
+mlp_f1 = f1_score(y_test, mlp_preds)
+logistic_f1 = f1_score(y_test, logistic_preds)
+
+# Print F1 scores
+print(f'SVM F1 Score: {svm_f1}')
+print(f'MLP F1 Score: {mlp_f1}')
+print(f'Logistic Regression F1 Score: {logistic_f1}')
