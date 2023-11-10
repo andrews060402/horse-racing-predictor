1c99bac5c114cdd65c81a7b6b7d16d9c5adc8e37
chungmjvt <chungmj@vt.edu>
2023-11-10 17:09:06 -0500

    top 3

--- src/horse-racing-predictor/michael_test.py
+++ src/horse-racing-predictor/michael_test.py
@@ -10,6 +10,7 @@ from sklearn.metrics import f1_score
 attributes = ['age', 'weight', 'sex', 'start_position', 'weather', 'track_conditions', 'finish_position']
 # Step 1: Load the CSV file into a pandas DataFrame
 df = pd.read_csv('../../data/woodbine_horses.csv', usecols=attributes)
+df['top_3_finish'] = df['finish_position'].apply(lambda x: 1 if x in [1, 2, 3] else 0)
 
 # Assuming 'sex', 'weather', and 'track_conditions' are categorical
 categorical_attributes = ['sex', 'weather', 'track_conditions']
@@ -29,16 +30,16 @@ df[numerical_attributes] = scaler.fit_transform(df[numerical_attributes])
 
 # Splitting the Data into Training and Testing Sets
 # Assuming you have a target variable named 'outcome'
-X = df.drop('finish_position', axis=1)
-y = df['finish_position']
+X = df.drop(['finish_position', 'top_3_finish'], axis=1)
+y = df['top_3_finish']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 # MODEL TRAINING AND EVALUATION
 # Initialize models
 svm = SVC()
-mlp = MLPClassifier()
-logistic = LogisticRegression()
+mlp = MLPClassifier(max_iter=8000, learning_rate_init=0.001)
+logistic = LogisticRegression(solver='lbfgs', max_iter=8000)
 
 # Train models
 svm.fit(X_train, y_train)
